6e4decc6928dcbce4379a974954ff456ac38f5a9
Mehedi Hasan Sarkar <mehedisarkar@hotmail.com>
2021-11-10 01:55:29 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex flex-col min-h-screen font-poppins">
+      <h1 className="font-grandHotel text-5xl font-medium tracking-wider text-red-500">
+        Hello Light Wars
+      </h1>
+      <p>
+        this is a paragraph.. gain a paragraph. How about you dear? how fell you
+        now?
+      </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        grandHotel: ["Grand Hotel", "cursive"],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
